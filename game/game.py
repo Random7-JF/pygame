3dd72d87c106065d2d7ad9e0a4c114db78ecb686
Jon Fingas <jon.fingas@gmail.com>
2024-10-14 21:21:34 -0500

    working on structure

---
+++ game/game.py
@@ -0,0 +1,32 @@
+import pygame
+from game.constants import *
+
+class Game():
+    def __init__(self):
+        self.delta_time = 0.0
+        self.clock = pygame.time.Clock()
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        
+
+    
+    def run(self):
+        while True:
+            self.screen.fill("black")
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    return
+            # Iterate over Object groups and call update
+            # Iterate over Object groups and call draw
+        
+            pygame.display.flip()
+            self.delta_time = (self.clock.tick(60) / 1000)
+
+    ##Add function to add object group to Draw calls
+    def add_group_to_draw(self, group):
+        pass
+    ##Add function to add object group to Update calls
+    def add_group_to_update(self, group):
+        pass
+    ##Add function to add object to object group
+    ##Remove Object from object group
+    ##Remove Object group from groups
--- main.py
+++ main.py
@@ -1,26 +1,8 @@
-import pygame
-from game.constants import *
-from object.player import *
+from game.game import *
 
 def main():
-    delta_time = 0.0
-    clock = pygame.time.Clock()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT * 0.90)
-
-    # Main Game Loop
-    while True:
-        screen.fill("black")
-        player.update(delta_time)
-        player.draw(screen)
-        
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return
-        
-        pygame.display.flip()
-        delta_time = (clock.tick(60) / 1000)
+    game = Game()
+    game.run()
 
 if __name__ == "__main__":
     main()
